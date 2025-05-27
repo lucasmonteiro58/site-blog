0e335b7142578b66eacc01d37676ffd63b755412
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-05-27 18:32:08 -0300

    feat: ✨ add author field to posts and integrate contentlayer in blog list

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -3,12 +3,16 @@ import { PostGridCard } from '@/components/post-grid-card'
 import { Search } from '@/components/search'
 import { useRouter } from 'next/router'
 
+import { allPosts } from 'contentlayer/generated'
+
 export function BlogList() {
   const router = useRouter()
   const query = router.query.q as string
   const pageTitle = query
     ? `Resultados de busca para "${query}"`
     : 'Dicas e estratégias para impulsionar seu negócio'
+
+  const posts = allPosts
 
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -31,17 +35,20 @@ export function BlogList() {
       </header>
 
       <PostGridCard>
-        <PostCard
-          title="Transformando seu negócio em uma loja virtual"
-          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online..."
-          date="20/12/2024"
-          slug="transformando"
-          image="/assets/primeiro-post.png"
-          author={{
-            avatar: '/customer-01.png',
-            name: 'Aspen Dokidis',
-          }}
-        />
+        {posts.map((post) => (
+          <PostCard
+            key={post._id}
+            title={post.title}
+            description={post.description}
+            date={new Date(post.date).toLocaleDateString('pt-BR')}
+            slug={post.slug}
+            image={post.image}
+            author={{
+              avatar: post.author.avatar,
+              name: post.author.name,
+            }}
+          />
+        ))}
       </PostGridCard>
     </div>
   )
