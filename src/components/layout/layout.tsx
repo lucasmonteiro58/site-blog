231979a178ac2f0ac11b3791be55e5488bbdfd67
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-05-27 11:25:52 -0300

    feat: ✨ add hero section component with SVG illustration and update styles

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,18 @@
-import { Header } from '../header'
+import { Inter } from 'next/font/google'
 import { Footer } from '../footer'
+import { Header } from '../header'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
+const inter = Inter({ subsets: ['latin'] })
+
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="relative flex min-h-screen flex-col dark ">
+    <div
+      className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}
+    >
       <Header />
       <main className="flex-1 flex flex-col mt-10 mb-12">{children}</main>
       <Footer />
