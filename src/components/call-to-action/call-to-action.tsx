197880bf98c565f2db89e1ae71707c0a69ac38b9
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-05-27 14:26:56 -0300

    feat: ✨ Add background SVGs and update layouts for sections

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -4,10 +4,12 @@ import { Button } from '../ui/button'
 
 export const CallToAction = () => {
   return (
-    <section className="mt-8 py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700 relative">
-      <div className="container">
+    <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="absolute inset-0 bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+
+      <div className="container relative">
         <div className="flex flex-col items-center gap-6 text-center ">
-          <div className="p-4 bg-cyan-300 w-fit rounded-full absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="p-4 bg-cyan-300 w-fit rounded-full ">
             <Store className="text-cyan-100" />
           </div>
 
