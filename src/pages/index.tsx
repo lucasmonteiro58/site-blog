445c24d6a068576719655f3048aa61cae0bce4e0
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-05-27 12:11:44 -0300

    feat: ✨ add support section component and integrate into home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { CustomerStorySection } from '@/components/customer-story-section'
 import { FeatureSection } from '@/components/feature-section'
 import { HeroSection } from '@/components/hero-section'
+import { SupportSection } from '@/components/support-section'
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
       <article className="flex flex-col">
         <HeroSection />
         <FeatureSection />
+        <SupportSection />
+        <CustomerStorySection />
       </article>
     </>
   )
