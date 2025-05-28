ccae0e7d942db2c5568283028b97ace3d36e3101
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-05-28 14:50:20 -0300

    feat: ✨ add layout and landing page components with global styles fix: 🐛 update active link logic to use usePathname for better routing

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { ActiveLink } from '../../active-link'
 import { Button } from '../../ui/button'
---
+++ src/pages/landing-page.tsx
@@ -0,0 +1,11 @@
+import { LandingPage } from '@/templates/landing-page'
+
+export default function Home() {
+  return (
+    <>
+      <article className="flex flex-col">
+        <LandingPage />
+      </article>
+    </>
+  )
+}
